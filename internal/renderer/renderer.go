6d094f2e5fd510b877e7010f0db5f5a05c92d9b5
omborate100 <omcode100@gmail.com>
2026-07-19 13:24:33 +0530

    add template renderer

--- internal/renderer/renderer.go
+++ internal/renderer/renderer.go
@@ -1,1 +1,36 @@
 package renderer
+
+import (
+	"bytes"
+	"html/template"
+)
+
+type TemplateRenderer struct{}
+
+func NewTemplateRenderer() *TemplateRenderer {
+	return &TemplateRenderer{}
+}
+
+func (r *TemplateRenderer) Render(
+	templateText string,
+	variables map[string]interface{},
+) (string, error) {
+
+	tmpl, err := template.New("email").
+		Option("missingkey=error").
+		Parse(templateText)
+
+	if err != nil {
+		return "", err
+	}
+
+	var output bytes.Buffer
+
+	err = tmpl.Execute(&output, variables)
+
+	if err != nil {
+		return "", err
+	}
+
+	return output.String(), nil
+}
