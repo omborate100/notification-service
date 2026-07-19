94009c0c8da52a970c872b9659aca0c90a0242d2
omborate100 <omcode100@gmail.com>
2026-07-19 13:20:23 +0530

    add repositry files to interact with db

--- internal/repository/notification_repository.go
+++ internal/repository/notification_repository.go
@@ -1,1 +1,119 @@
 package repository
+
+import (
+	"context"
+	"encoding/json"
+
+	"notification-service/internal/model"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type NotificationRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewNotificationRepository(db *pgxpool.Pool) *NotificationRepository {
+	return &NotificationRepository{
+		db: db,
+	}
+}
+func (r *NotificationRepository) Create(
+	notification *model.EmailNotification,
+) (int64, error) {
+
+	variablesJSON, err := json.Marshal(notification.Variables)
+
+	if err != nil {
+		return 0, err
+	}
+
+	query := `
+	INSERT INTO email_notifications
+	(
+		template_id,
+		recipient_email,
+		subject,
+		body,
+		variables,
+		status,
+		provider
+	)
+	VALUES
+	(
+		$1,$2,$3,$4,$5,$6,$7
+	)
+	RETURNING id
+	`
+
+	var id int64
+
+	err = r.db.QueryRow(
+		context.Background(),
+		query,
+		notification.TemplateID,
+		notification.RecipientEmail,
+		notification.Subject,
+		notification.Body,
+		variablesJSON,
+		notification.Status,
+		notification.Provider,
+	).Scan(&id)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (r *NotificationRepository) MarkSent(
+	id int64,
+	providerMessageID string,
+) error {
+
+	query := `
+	UPDATE email_notifications
+	SET
+		status=$1,
+		provider_message_id=$2,
+		sent_at=NOW(),
+		updated_at=NOW()
+	WHERE id=$3
+	`
+
+	_, err := r.db.Exec(
+		context.Background(),
+		query,
+		model.StatusSent,
+		providerMessageID,
+		id,
+	)
+
+	return err
+}
+
+func (r *NotificationRepository) MarkFailed(
+	id int64,
+	errorMessage string,
+) error {
+
+	query := `
+	UPDATE email_notifications
+	SET
+		status=$1,
+		error_message=$2,
+		updated_at=NOW()
+	WHERE id=$3
+	`
+
+	_, err := r.db.Exec(
+		context.Background(),
+		query,
+		model.StatusFailed,
+		errorMessage,
+		id,
+	)
+
+	return err
+}
--- internal/repository/template_repository.go
+++ internal/repository/template_repository.go
@@ -1,1 +1,63 @@
 package repository
+
+import (
+	"context"
+	"errors"
+
+	"notification-service/internal/model"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type TemplateRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewTemplateRepository(db *pgxpool.Pool) *TemplateRepository {
+	return &TemplateRepository{
+		db: db,
+	}
+}
+
+func (r *TemplateRepository) GetByTemplateKey(templateKey string) (*model.EmailTemplate, error) {
+
+	query := `
+	SELECT
+		id,
+		template_key,
+		template_name,
+		subject_template,
+		body_template,
+		description,
+		is_active,
+		created_at,
+		updated_at
+	FROM email_templates
+	WHERE template_key = $1
+	AND is_active = true
+	`
+
+	var template model.EmailTemplate
+
+	err := r.db.QueryRow(
+		context.Background(),
+		query,
+		templateKey,
+	).Scan(
+		&template.ID,
+		&template.TemplateKey,
+		&template.TemplateName,
+		&template.SubjectTemplate,
+		&template.BodyTemplate,
+		&template.Description,
+		&template.IsActive,
+		&template.CreatedAt,
+		&template.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, errors.New("template not found")
+	}
+
+	return &template, nil
+}
